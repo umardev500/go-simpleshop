32d6161adf4372be35d34378bd436f9a574c0b8a
umar <umar.dev500@gmail.com>
2023-11-24 20:01:06 +0700

    update migration

--- migrations/20231124123701_product.up.sql
+++ migrations/20231124123701_product.up.sql
@@ -2,7 +2,9 @@ CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
-    stock INT NOT NULL DEFAULT 0
+    stock INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 INSERT INTO products (name, price, stock)
 VALUES ('Product 1', 19.99, 5),
