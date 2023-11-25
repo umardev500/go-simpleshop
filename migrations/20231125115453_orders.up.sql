d94b0dfcf1f94cdae5f4a34abd6149fc721c6a7e
umar <umar.dev500@gmail.com>
2023-11-25 19:33:33 +0700

    update migration order

--- migrations/20231125115453_orders.up.sql
+++ migrations/20231125115453_orders.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS orders (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(user_id), -- Add user_id column
-    order_number SERIAL NOT NULL,
+    user_id INT NOT NULL REFERENCES users(user_id),
+    order_number VARCHAR(50) NOT NULL,
+    total DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
     status VARCHAR(50) NOT NULL DEFAULT 'pending',
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
